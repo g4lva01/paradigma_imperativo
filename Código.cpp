f4ab5eda4cd8f233ab073b5be85709358e089900
valloo-henriquemoura <126253616+valloo-henriquemoura@users.noreply.github.com>
2024-11-19 19:20:39 -0300

    Add files via upload

---
+++ Código.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int soma =0;
+		soma +=1;
+		soma +=2;
+		soma +=3;
+		soma +=4;
+		soma +=5;
+		soma +=6;
+		soma +=7;
+		soma +=8;
+		soma +=9;
+		soma +=10;
+		
+		printf("%i",soma);
+	return 0;
+}
